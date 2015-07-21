1b7f496b2d3f48942060cebbd21cf9dc8be8fb0b
Edwin Hernández Cabrera <edinfamous@gmail.com>
2015-07-21 18:13:23 -0500

    Ajustes en detalle de equipo

--- sites/all/modules/custom/ligadtv/theme/matchs_detail.tpl.php
+++ sites/all/modules/custom/ligadtv/theme/matchs_detail.tpl.php
@@ -1,4 +1,3 @@
-<?php dpm($idMatchs);?>
 <div class="gameEstadisticas">
 <?php foreach ($matchs as $key => $value) { ?>
   <?php if($value['matchId']==$idMatchs):?>
@@ -12,11 +11,17 @@
     </div>
     <div class="gmStaTeam">
         <div class="gmStaScore"><?php print $value['competitors'][1]['scoreString'];?></div>
-        <div class="gmStaname"><?php print $value['competitors'][1]['clubName'];?></div>
+        <div class="gmStaname"><?php print $value['competitors'][1]['teamName'];?></div>
         <div class="ico1"><img src="<?php print $value['competitors'][1]['images']['logo']['T1']['url'];?>"></div>
     </div>
     <div class="ccStaInfoC large-12 columns">
         <p><?php print $prGame['venue'] . date( 'H:i', strtotime( $value['matchTime'] )) . date( 'd-m-Y', strtotime( $value['matchTime'] ))?> </p>
+    </div>
+    <div class="large-12 columns titleEsGm">
+        <?php print $value['competitors'][0]['teamName'];?>
+    </div>
+    <div class="large-12 columns titleEsGm final">
+        <?php print $value['competitors'][1]['teamName'];?>
     </div>
   <?php endif?>
 <?php } ?>
