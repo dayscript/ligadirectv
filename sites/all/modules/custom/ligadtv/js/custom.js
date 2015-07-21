0b49162075ee20b23b00ef0b5571627988b87cbf
Edwin Hernández Cabrera <edinfamous@gmail.com>
2015-07-21 14:01:34 -0500

    Cambios para optimizar estadisticas y que sea asincrona

--- sites/all/modules/custom/ligadtv/js/custom.js
+++ sites/all/modules/custom/ligadtv/js/custom.js
@@ -1,13 +1,31 @@
 Drupal.behaviors.custom = {
   attach: function (context, settings) {
-    var base = window.location.host;
-    var leader = loadJson( base, 'sPointsAverage.json');
-
-    console.log(base);
+    var leader   = loadJson('sPointsAverage.json','mediaPointHtml', 'MP');
+    var pointsReb= loadJson('sReboundsTotalAverage.json','mediaRebotes', 'REBM');
+    var pointsAsisten = loadJson('sAssistsAverage.json','mediaAsistence','MAS');
+    var pointsTL = loadJson('sFreeThrowsPercentage.json' ,'tlporcentaje', 'TL');
+    var points3  = loadJson('sFreeThrowsPercentage.json','3porcentaje', 'T3');
+    var points2  = loadJson('sTwoPointersPercentage.json', '2porcentaje' ,'T2');
+    var points   = loadJson('sPoints.json', 'puntos', 'PT');
+    var pointsT  = loadJson('sReboundsTotal.json','rebot-total', 'RT');
+    var pointsAs = loadJson('sAssists.json', 'asistencias', 'AS');
+    var pointsRO = loadJson('sReboundsOffensive.json', 'rebotes-ofensivos', 'RO');
+    var pointsRD = loadJson('sReboundsDefensive.json', 'rebotes-defensivos', 'RD');
+    var pointsTA = loadJson('sBlocks.json', 'tapones', 'TAP');
+    var pointsTC = loadJson('sTrueShootingAttempts.json', 'tl-convertidos', 'TLC');
+    var recupero = loadJson('sSteals.json', 'recupero', 'REC');
+    var canasta2 = loadJson('sTwoPointersMade.json', 'canasta2', 'T2C');
+    var canasta3 = loadJson('sThreePointersMade.json', 'canasta3', 'T3C');
+    var faltasPer= loadJson('sFoulsPersonal.json', 'faltas-personales', 'FP');
+    var mediaRob = loadJson('sStealsAverage.json', 'media-robos', 'MR');
+    var eficienc = loadJson('sEfficiency.json', 'eficiencia', 'EF');
+    var mediaTap = loadJson('sBlocksAverage.json', 'media-tapones', 'MT');
+    var balPerdi = loadJson('sTurnovers.json', 'balones-perdidos', 'BP');
   }
 };
 
-function loadJson (base, name) {
+function loadJson (name, id, point) {
+  //var data_file = '/ligadirectv/sites/all/modules/custom/ligadtv/data/' + name; Descomentar en caso de ser local
   var data_file = '/sites/all/modules/custom/ligadtv/data/' + name;
   var http_request = new XMLHttpRequest();
   try{
@@ -31,13 +49,39 @@ function loadJson (base, name) {
       if (http_request.readyState == 4  ){
         // Javascript function JSON.parse to parse JSON data
         var jsonObj = JSON.parse(http_request.responseText);
-        console.log(jsonObj);
-        // jsonObj variable now contains the data structure and can
-        // be accessed as jsonObj.name and jsonObj.country.
-        //document.getElementById("Name").innerHTML =  jsonObj.name;
-        //document.getElementById("Country").innerHTML = jsonObj.country;
+        var leader  = Array.prototype.slice.call(jsonObj.response.data, 0);
+        var data = drawLeader(leader,point);
+        document.getElementById(id).innerHTML = data;
       }
    }
    http_request.open("GET", data_file, true);
    http_request.send();
 }
+/**
+ * [drawLeader description]
+ * @param  {string} leader [description]
+ * @param  {string} point  [description]
+ * @return {[type]}        [description]
+ */
+function drawLeader(leader, point) {
+  console.log(leader);
+  console.log(point);
+  for (var i = 0; i <= 5 ; i++) {
+    if(i == 0){
+      if(typeof(leader[i].images) != 'undefined'){
+        var data = '<div class="dataPlayer"><div class="leaderImg"><img src="' + leader[i].images.photo.T1.url + '"><div class="titleLidder">Leader</div></div><div class="info"><div class="inInfo"><h4>' + leader[i].firstName + ' ' + leader[i].familyName + '</h4><h5>' + leader[i].teamName + '</h5></div><div class="cc1Info"><div class="cc2Info">' +
+                            leader[i].sPoints + ' ' +point + '</div><img src="{$escudo}"></div></div></div><div class="tableLeader"><table><thead><tr><td width="20px">#</td><td width="50px">EQU</td><td width="185px" style="text-align: left;">NOMBRE</td><td width="45px">'+ point + '</td></tr></thead><tbody>';
+      }else{
+        var data = '<div class="dataPlayer"><div class="leaderImg"><img src=""><div class="titleLidder">Leader</div></div><div class="info"><div class="inInfo"><h4>' + leader[i].firstName + ' ' + leader[i].familyName + '</h4><h5>' + leader[i].teamName + '</h5></div><div class="cc1Info"><div class="cc2Info">' +
+                            leader[i].sPoints + point + '</div><img src="{$escudo}"></div></div></div><div class="tableLeader"><table><thead><tr><td width="20px">#</td><td width="50px">EQU</td><td width="185px" style="text-align: left;">NOMBRE</td><td width="45px">'+ point + '</td></tr></thead><tbody>';
+      }
+    }else{
+      data +='<tr><td>' + i + '</td><td>' + leader[i].teamCode + '</td><td>'+
+            leader[i].firstName  + leader[i].familyName +'</td><td>' +
+            leader[i].sPoints + '</td></tr>';
+    }
+  }
+  data += '</tbody></table><a href="/statistics/sPoints/'+ point + '" class="verTodos">VER LISTA COMPLETA >> </a></div>';
+  return data;
+}
+
