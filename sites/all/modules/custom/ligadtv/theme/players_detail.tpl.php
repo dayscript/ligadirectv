eb1a4a1097e6ddf917f0798707417c9301dbe446
Edwin Hernández Cabrera <edinfamous@gmail.com>
2015-07-21 06:32:07 -0500

    Detalle jugador

---
+++ sites/all/modules/custom/ligadtv/theme/players_detail.tpl.php
@@ -0,0 +1,42 @@
+<?php dpm($player);?>
+<?php $count = 0;?>
+<?php foreach ($player as $key => $value) { ?>
+  <?php if($value['personId'] == $idPlayer):?>
+    <?php if($count == 0):?>
+      <div class="playerInfoBig">
+        <div class="pipPic">
+          <img src="<?=$value['images']['photo']['S1']['url']?>">
+        </div>
+        <div class="sspib">
+          <img src="./img/escudo.png">
+          <img src="./img/logo_directv_calendario.png">
+        </div>
+        <?php drupal_set_title('NOMINA');?>
+        <div class="cinpib">
+          <h3><?=$value['firstName']?> <?=$value['familyName']?></h3>
+          <h2><?=$tes['title']?></h2>
+          <h3>ESTADÍSTICAS DE LA TEMPORADA</h2>
+          <div class="ccInfoPib ccipibFirts">
+            PPP: <?= $value['sPoints'] ?>
+          </div>
+          <div class="ccInfoPib">
+            RPG: <?= $value['sReboundsTotalAverage']?>
+          </div>
+          <div class="ccInfoPib">
+            APP: <?= $value['sAssistsPercentage']?>
+          </div>
+          <div class="ccInfoPib">
+            %TC: <?= $value['sThreePointersPercentage']?>
+          </div>
+          <div class="linepip"></div>
+        </div>
+      </div>
+      <div class="ccsponsor34">
+        <img src="/img/pauta1.png">
+        <img src="./img/pauta2.png">
+      </div>
+      <div class="space20"></div>
+      <?php $count = $count + 1;?>
+    <?php endif?>
+  <?php endif?>
+<?php } ?>
