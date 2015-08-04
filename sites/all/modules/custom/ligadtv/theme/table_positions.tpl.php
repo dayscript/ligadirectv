e62fe969e62e721bd43e30df380198c3e6c57c02
Edwin Hernández Cabrera <edinfamous@gmail.com>
2015-08-04 15:54:47 -0500

    Ajustes home

--- sites/all/modules/custom/ligadtv/theme/table_positions.tpl.php
+++ sites/all/modules/custom/ligadtv/theme/table_positions.tpl.php
@@ -16,50 +16,10 @@
                 </tr>
             </table>
             <table>
-                <tr class="subtitle">
-                    <td>GRUPO A</td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                </tr>
                 <?php
                 $cond = false;
                 foreach ($items[0] as $key => $value) { ?>
-                    <?php if($value['poolNumber'] == '1' && $value['roundNumber'] == $items[1]):?>
-                        <?php if($cond): ?>
-                            <tr class='tdIn'>
-                                <td><?php print $value['clubName']; ?></td>
-                                <td><?php print $value['played'];?></td>
-                                <td><?php print $value['won'];?></td>
-                                <td><?php print $value['lost'];?></td>
-                                <td><?php print (($value['won'] * 2) + $value['lost']); ?></td>
-                            </tr>
-                        <?php else:?>
-                            <tr class="tdIn naranja">
-                                <td><?php print $value['clubName']; ?></td>
-                                <td><?php print $value['played']; ?></td>
-                                <td><?php print $value['won']; ?></td>
-                                <td><?php print $value['lost']; ?></td>
-                                <td><?php print (($value['won'] * 2) + $value['lost']); ?></td>
-                            </tr>
-                        <?php endif ?>
-                        <?php $cond = $cond ? false : true;?>
-                    <?php endif ?>
-                <?php } ?>
-            </table>
-             <table>
-                <tr class="subtitle">
-                    <td>GRUPO B</td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                </tr>
-                <?php
-                $cond = false;
-                foreach ($items[0] as $key => $value) { ?>
-                    <?php if($value['poolNumber'] == '2' && $value['roundNumber'] == $items[1]):?>
+                    <?php if($value['roundNumber'] == $items[1]):?>
                         <?php if($cond): ?>
                             <tr class='tdIn'>
                                 <td><?php print $value['clubName']; ?></td>
