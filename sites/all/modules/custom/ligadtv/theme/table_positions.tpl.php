eeaccc9a7871d68b2c69e5796e7739cf7ac5ea62
ftorresdayscript <ftorres@dayscript.com>
2015-08-12 17:12:45 -0500

    changes

--- sites/all/modules/custom/ligadtv/theme/table_positions.tpl.php
+++ sites/all/modules/custom/ligadtv/theme/table_positions.tpl.php
@@ -14,13 +14,11 @@
                     <td>P</td>
                     <td>PTS</td>
                 </tr>
-            </table>
-            <table>
                 <?php
                 $cond = false;
                 foreach ($items[0] as $key => $value) { ?>
                     <?php if($value['roundNumber'] == $items[1]):?>
-                        <?php if($cond): ?>
+                       <?php /*if($cond): ?>
                             <tr class='tdIn'>
                                 <td><?php print $value['clubName']; ?></td>
                                 <td><?php print $value['played'];?></td>
@@ -36,8 +34,27 @@
                                 <td><?php print $value['lost']; ?></td>
                                 <td><?php print (($value['won'] * 2) + $value['lost']); ?></td>
                             </tr>
+                        <?php endif */?>
+                        <?php if($cond): ?>
+                            
+                            <tr class='tdIn'>
+                                <td><?php print $value['clubName']; ?></td>
+                                <td><?php print $value['played'];?></td>
+                                <td><?php print $value['won'];?></td>
+                                <td><?php print $value['lost'];?></td>
+                                <td><?php print (int)$value['standingPoints']; ?></td>
+                            </tr>
+                        <?php else:?>
+                            <tr class="tdIn naranja">
+                                <td><?php print $value['clubName']; ?></td>
+                                <td><?php print $value['played'];?></td>
+                                <td><?php print $value['won'];?></td>
+                                <td><?php print $value['lost'];?></td>
+                                <td><?php print (int)$value['standingPoints']; ?></td>
+                            </tr>
                         <?php endif ?>
-                        <?php $cond = $cond ? false : true;?>
+                       
+                        <?php $cond = $cond ? false : true; ?>
                     <?php endif ?>
                 <?php } ?>
             </table>
